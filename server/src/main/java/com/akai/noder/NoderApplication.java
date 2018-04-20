31b65ca7fbe81ce055645199d03ca75c39cfa1c5
Kudaibergen <tuleubekov.kas@gmail.com>
2018-04-20 17:16:47 +0600

    first commit

---
+++ server/src/main/java/com/akai/noder/NoderApplication.java
@@ -0,0 +1,12 @@
+package com.akai.noder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoderApplication.class, args);
+    }
+}
