84062c17fb9cd133e0f1bd23eb8b6f0f1a8e8fe6
Kudaibergen <tuleubekov.kas@gmail.com>
2018-05-22 20:08:39 +0600

    Add logic in base presenter

--- android-app/app/src/main/java/com/akai/noder/app/ui/base/BasePresenter.java
+++ android-app/app/src/main/java/com/akai/noder/app/ui/base/BasePresenter.java
@@ -2,5 +2,21 @@ package com.akai.noder.app.ui.base;
 
 public abstract class BasePresenter<V extends ContractView> implements ContractPresenter<V> {
 
+    private V mContractView;
+
     protected BasePresenter(){}
+
+    protected V getContractView() {
+        return mContractView;
+    }
+
+    @Override
+    public void onAttach(V contractView) {
+        this.mContractView = contractView;
+    }
+
+    @Override
+    public void onDetach() {
+        mContractView = null;
+    }
 }
--- android-app/app/src/main/java/com/akai/noder/app/ui/base/ContractPresenter.java
+++ android-app/app/src/main/java/com/akai/noder/app/ui/base/ContractPresenter.java
@@ -1,4 +1,8 @@
 package com.akai.noder.app.ui.base;
 
 public interface ContractPresenter<V extends ContractView> {
+
+    void onAttach(V contractView);
+
+    void onDetach();
 }
