9e5bd658c1bbac157d9f2b648f8344e03f60e51f
Kudaibergen <tuleubekov.kas@gmail.com>
2018-05-22 17:39:26 +0600

    Fix minor

--- android-app/app/src/main/java/com/akai/noder/app/ui/main/ContractMainPresenter.java
+++ android-app/app/src/main/java/com/akai/noder/app/ui/main/ContractMainPresenter.java
@@ -3,6 +3,7 @@ package com.akai.noder.app.ui.main;
 import com.akai.noder.app.ui.base.ContractPresenter;
 
 public interface ContractMainPresenter<V extends ContractMainView> extends ContractPresenter<V> {
+
     void fabPersonalClick();
 
     void fabSecretClick();
